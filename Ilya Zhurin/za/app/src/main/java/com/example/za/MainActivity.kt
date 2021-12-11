fff3cedfb2ed913989e4e4bf3ac86aa791504a18
Ilya Zhurin <zhurin773@gmail.com>
2021-12-11 14:04:14 +0300

    Check code3

--- Ilya Zhurin/za/app/src/main/java/com/example/za/MainActivity.kt
+++ Ilya Zhurin/za/app/src/main/java/com/example/za/MainActivity.kt
@@ -16,20 +16,18 @@ class MainActivity : AppCompatActivity() {
             val email = binding.enterEmail.text.toString()
             val password = binding.confirmPassword.text.toString()
             if (validatorEror.email_all(email) &&
-                validatorEror.enterPassword_regestration(password) == true)
-            {
-                val intent = Intent(this,ProfileActivity::class.java)
-                intent.putExtra("name", email)
-                startActivity(intent)
+                validatorEror.enterPassword_regestration(password) == true
+            ) {
+                    val intent = Intent(this,ProfileActivity::class.java)
+                    intent.putExtra("name", email)
+                    startActivity(intent)
             }
             else {
                 if (validatorEror.email_all(email) == false)
                 {
-
                     binding.enterEmail.error = "Введена не коректная почта"}
                     if (validatorEror.enterPassword_regestration(password) == false)
                     {
-
                         binding.confirmPassword.error = "Вы ввели не коректный пароль"
                     }
                 }
