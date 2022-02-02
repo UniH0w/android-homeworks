0528272e18ede9e11db06e19561a655961d5a801
Ilya Zhurin <zhurin773@gmail.com>
2022-02-02 19:31:04 +0300

    Fixing  mail transfers

--- Ilya Zhurin/za/app/src/main/java/com/example/za/fragments/ProfileFragment.kt
+++ Ilya Zhurin/za/app/src/main/java/com/example/za/fragments/ProfileFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.za.R
 import com.example.za.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
@@ -15,7 +16,10 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentProfileBinding.inflate(inflater, container, false)
-        binding.profileExit.setOnClickListener { activity?.finish() }
+        binding.profileExit.setOnClickListener { activity?.finish()
+        }
+        val name = activity?.intent?.extras?.getString("Name")
+        binding.profileText.text = getString(R.string.profile_greeting, name)
         return binding.root
     }
 }
